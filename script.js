bb6f6e52b77f9aa42c4e3eacc60d2c1704ef4db5
Glimpz <abgabbard@gmail.com>
2021-05-29 19:00:02 -0700

    added random items

--- script.js
+++ script.js
@@ -2,6 +2,30 @@
 //     onclick()
 // }
 
+// function refresher() {
+//     document.getElementById("refresh").inner 
+// }
+
 function freshClicker() {
+
     document.getElementById("btnone").innerHTML = "Fresh Clicker";
+  }
+function colorChanger() {
+    document.getElementById("btnthree").style.color = "purple"
+  }
+function rambler() {
+    document.getElementById("btnfour").innerHTML = "Refresh";
+    alert("listen to me chat!!")
+  }
+function gambler() {
+    document.getElementById("btnfive").innerHTML = "Refresh";
+  }
+function runner() {
+    document.getElementById("btnsix").innerHTML = "Refresh";
+  }
+function foodSelector() {
+    document.getElementById("btnseven").innerHTML = "Refresh";
+  }
+function randogeno() {
+    document.getElementById("btneight").innerHTML = "Refresh";
   }
